51d5e3b280c2e26ecf2da6d411f10d319b22e4e4
CarlosYcochea <134462386+CarlosYcochea@users.noreply.github.com>
2024-07-03 00:41:24 -0400

    migracion 1

--- ventas/views.py
+++ ventas/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'ventas/index.html')
+
+def login(request):
+
+    return render(request, 'ventas/login.html')
+
+def dashboard(request):
+
+    return render(request, 'ventas/dashboard.html')
+
+def clientes(request):  
+
+    return render(request, 'ventas/clientes.html')
