0ca7725efeabadd700525f9d7430064edf62d5b2
CarlosYcochea <134462386+CarlosYcochea@users.noreply.github.com>
2024-07-05 15:19:57 -0400

    migration, makemigration y crud de clientes

--- ventas/models.py
+++ ventas/models.py
@@ -22,24 +22,24 @@ class Ventas(models.Model):
     id_ventas = models.AutoField(db_column='idVentas',primary_key=True)
     rut = models.ForeignKey('Clientes', on_delete=models.CASCADE,db_column='rut')
     fechaventas = models.DateField(blank=False, null=False)
-    total = models.IntegerField(blank=False, null=False)
+    total = models.FloatField(default=0.0,blank=False,null=False)
     def __str__(self):
         return str(self.fechaPedido)
 
-class Produtos(models.Model):
+class Productos(models.Model):
     id_producto = models.AutoField(db_column='idProducto',primary_key=True)
     nombre = models.CharField(max_length=20)
     descripcion = models.CharField(max_length=30)
-    precio = models.IntegerField(blank=False,null=False)
-    id_categorias = models.ForeignKey('Categorias', on_delete=models.CASCADE,db_column='idCategorias')
+    precio = models.FloatField(default=0.0,blank=False,null=False)
+    id_categorias = models.ForeignKey('Categorias', on_delete=models.CASCADE, db_column='idCategorias')
     def __str__(self):
         return str(self.nombre)
 
 class DetalleVentas(models.Model):
     id_detalle_ventas = models.AutoField(db_column='idDetalleVentas',primary_key=True)
-    id_ventas = models.ForeignKey('Ventas', on_delete=models.CASCADE,db_column='idVentas')
-    id_productos = models.ForeignKey('Productos', on_delete=models.CASCADE,db_column='idProductos')
+    id_ventas = models.ForeignKey('Ventas', on_delete=models.CASCADE, db_column='idVentas')
+    id_producto = models.ForeignKey('Productos', on_delete=models.CASCADE , db_column='idProducto')
     cantidad = models.IntegerField(blank=False,null=False)
-    precio = models.IntegerField(blank=False,null=False)
+    precio = models.FloatField(default=0.0,blank=False,null=False)
     def __str__(self):
         return str(self.cantidad)
--- ventas/views.py
+++ ventas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Clientes, Categorias, Ventas, DetalleVentas, Productos
 
 # Create your views here.
 
@@ -15,5 +16,90 @@ def dashboard(request):
     return render(request, 'ventas/dashboard.html')
 
 def clientes(request):  
+    clientes = Clientes.objects.all()
+    context = {"clientes" :clientes}
+    return render(request, 'ventas/clientes.html',context)
 
-    return render(request, 'ventas/clientes.html')
+def clientesAdd(request):
+    if  request.method !="POST":
+        
+        ventas = Ventas.objects.all()
+        context={'ventas':ventas}
+        return render(request, 'ventas/clientes_add.html', context)
+    elif  request.method =="POST":
+
+
+
+        rut=request.POST["rut"]
+        nombre=request.POST["nombre"]
+        apellido=request.POST["apellido"]
+        telefono=request.POST["telefono"]
+        email=request.POST["email"]
+
+        obj=Clientes.objects.create(  rut=rut,
+                                    nombre=nombre,
+                                    apellido=apellido,
+                                    telefono=telefono,
+                                    email=email
+                                    )
+        obj.save()
+        context={'mensaje':"OK, datos grabados..."}
+        return render(request, 'ventas/clientes_add.html', context)
+    
+def clientes_del(request,pk):
+    context={}
+    try:
+        cliente=Clientes.objects.get(rut=pk)
+
+        cliente.delete()
+        mensaje="Bien, datos eliminados..."
+        clientes = Clientes.objects.all()
+        context = {'clientes': clientes,  'mensaje' : mensaje}
+        return render(request, 'ventas/clientes.html', context)
+    except:
+        mensaje="Error, rut no existe..."
+        clientes = Clientes.objects.all()
+        context = {'clientes': clientes,  'mensaje' : mensaje}
+        return render(request, 'ventas/clientes.html', context)
+
+def clientes_findEdit(request,pk):
+
+    if pk != "":
+        cliente=Clientes.objects.get(rut=pk)
+
+
+        context={'cliente':cliente}
+        if cliente:
+            return render(request, 'ventas/clientes_edit.html', context)
+        else:
+            context={'mensaje':"Error, rut no existe..."}
+            return render(request, 'ventas/clientes.html', context)
+        
+
+def clientesUpdate(request):
+
+    if request.method == "POST":
+
+
+        rut=request.POST["rut"]
+        nombre=request.POST["nombre"]
+        apellido=request.POST["apellido"]
+        telefono=request.POST["telefono"]
+        email=request.POST["email"]
+
+
+        cliente = Clientes()
+        cliente.rut=rut
+        cliente.nombre=nombre
+        cliente.apellido=apellido
+        cliente.telefono=telefono
+        cliente.email=email
+        cliente.save()
+
+        context={'mensaje':"Ok, datos actualizados...",'cliente':cliente }
+        return render(request, 'ventas/clientes_edit.html', context)
+    else:
+
+        clientes = Clientes.objects.all()
+        context={'clientes':clientes}
+        return render(request, 'ventas/clientes_edit.html', context)    
